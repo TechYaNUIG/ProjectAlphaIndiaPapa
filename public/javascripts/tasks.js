709926437d055ffedc7eed3accf89a104b50b0ec
Aideen McLoughlin <37660493+a-mcloughlin@users.noreply.github.com>
2019-02-21 23:28:46 +0000

    Adding task creator names to tasks

--- public/javascripts/tasks.js
+++ public/javascripts/tasks.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
             success: function (data) {
                 var tasks = "<ul class='tasks-list'>";
                 for (var i = 0; i < data.length; i++) {
-                    tasks+= "<li class='list-item'><label class='check-container'><h4>" + data[i].task + "</h4><input type='checkbox' id='check-complete' name='"+ data[i]._id +"'";
+                    tasks+= "<li class='list-item'><label class='check-container'><h4>" + data[i].task + "<div class=\"task-user-name\">"+"Created by: "+ data[i].user_name + "</div>"+"</h4><input type='checkbox' id='check-complete' name='"+ data[i]._id +"'";
                     if(data[i].commpleted){
                         tasks+= "checked";
                     }  
