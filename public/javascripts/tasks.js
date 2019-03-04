3cd8b9ab7c7a8cca8505be76d9bf5ed29779d04f
Aideen McLoughlin <37660493+a-mcloughlin@users.noreply.github.com>
2019-03-04 17:28:27 +0000

    Adding due dates- Still need to sort out formatting

--- public/javascripts/tasks.js
+++ public/javascripts/tasks.js
@@ -19,9 +19,12 @@ $(document).ready(function () {
             type: 'GET',
             success: function (data) {
                 var tasks = "<ul class='tasks-list'>";
+		var due = "";
                 for (var i = 0; i < data.length; i++) {
-                    tasks += "<li class='list-item'><label class='check-container'><h4>" + data[i].task + "<div class=\"task-user-name\">" + "Created by: " + data[i].user_name + "</div>" + "</h4><input type='checkbox' id='check-complete' name='" + data[i]._id + "'";
-                    if (data[i].commpleted) {
+		if(data[i].date_due!=null){due = "Due by: "+data[i].date_due;}
+                    tasks+= "<li class='list-item'><label class='check-container'><h4>" + data[i].task + "<div class=\"task-user-name\">"+"Created by: "+ data[i].user_name +"<br><br>"+due  + "</div>"+"</h4><input type='checkbox' id='check-complete' name='"+ data[i]._id +"'";
+                    due="";
+		    if(data[i].commpleted){
                         tasks += "checked";
                     }
                     tasks += "><span class='checkmark'></span></label></li>";
@@ -37,11 +40,12 @@ $(document).ready(function () {
         $.ajax({
             url: '/addTask/',
             type: 'POST',
-            data: { task: $('#taskInput').val() },
+            data: { task: $('#taskInput').val(), date_due:$('#taskDeadline').val() },
             success: function (data) {
                 getTasks();
                 addTaskMessage();
                 $("#taskInput").val("")
+		due_date:$('#taskDeadline').val("")
             }
         });
     });
