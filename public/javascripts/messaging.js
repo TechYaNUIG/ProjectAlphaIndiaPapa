b4e614a050657661e2de9155c8bd7101de7c78cb
robwalshdev <robwalshdev@gmail.com>
2019-03-16 18:27:39 +0000

    User page styling, stop reload on message send and scroll to newest message

--- public/javascripts/messaging.js
+++ public/javascripts/messaging.js
@@ -1,45 +1,59 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
     userHasScrolled = false;
     var currTeam = localStorage.getItem("currentTeam");
-    getMessages();    
+    getMessages();
 
-    $('#send-message').click(function(event){
+    $('#send-message').click(function (event) {
+        event.preventDefault();
         currTeam = localStorage.getItem("currentTeam");
         var messageText = $('#message-text').val();
         $.ajax({
-            type:'POST',
-            url:'/add-message/'+currTeam,
-            data:{'text':messageText},
-            success:function(data){
+            type: 'POST',
+            url: '/add-message/' + currTeam,
+            data: { 'text': messageText },
+            success: function (data) {
                 $('#message-text').val("");
                 getMessages();
             }
         });
+        scrollToBottom();
     });
 
-    function getMessages(){
+    function getMessages() {
         currTeam = localStorage.getItem("currentTeam");
         $.ajax({
             type: "GET",
-            url:"/get-messages/"+ currTeam,
-            success:function(data){
+            url: "/get-messages/" + currTeam,
+            success: function (data) {
                 var template = Handlebars.templates['messages'];
-                var templateData = template({messages:data});
+                var templateData = template({ messages: data });
                 $('#messages').html(templateData);
             }
         });
-        setTimeout(getMessages, 2000);
         scrollToBottom();
+        setTimeout(getMessages, 2000);
     }
 
-    function scrollToBottom(){
-        if(!userHasScrolled){
+    function scrollToBottom() {
+        if (!userHasScrolled) {
             $('.message-section').scrollTop($('.message-section')[0].scrollHeight);
+            $(".message-section").animate({
+                scrollTop: $(".message-section")[0].scrollHeight
+            }, "slow");
         }
     }
 
-     $('.message-section').scroll(function(){
-        userHasScrolled = true;
-     });
+    var lastScrollTop = 0;
+
+    $('.message-section').scroll(function () {
+        var st = $(this).scrollTop();
+        if (!(st > lastScrollTop)){
+            userHasScrolled = true;
+        } else if(st = $(".message-section")[0].scrollHeight){
+            userHasScrolled = false;
+        }
+        lastScrollTop = st;
+    });
+
 });
